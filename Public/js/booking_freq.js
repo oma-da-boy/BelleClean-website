5ae3882df7d73316deb93d42451e3099612ef108
oma-da-boy <74117237+oma-da-boy@users.noreply.github.com>
2023-04-03 11:38:58 +0100

    Update booking_freq.js

--- Public/js/booking_freq.js
+++ Public/js/booking_freq.js
@@ -1,6 +1,7 @@
 const services = document.getElementById("service_type")
 const commercial = document.getElementById("commercial_service")
 
+// setting options for testarea
 var text = document.createElement("textarea")
 text.cols = 30
 text.rows = 4
@@ -9,6 +10,7 @@ text.placeholder = "What kind of business is it?"
 commercial.appendChild(text)
 commercial.style.display = "none";
 
+// displays a testbox when commerial is selected for service type
 services.addEventListener("change", function (e) {
     commercial.style.display = "none";
     if (services.value == "commercial") {
